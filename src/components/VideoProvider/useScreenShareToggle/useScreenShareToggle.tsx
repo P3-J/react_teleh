efd2801f603b4d0d7bd179dd76b1dd5aeb02143d
P3-J <jako.keskkyla@gmail.com>
2023-07-20 17:18:41 +0300

    screenshareaudio fix

--- src/components/VideoProvider/useScreenShareToggle/useScreenShareToggle.tsx
+++ src/components/VideoProvider/useScreenShareToggle/useScreenShareToggle.tsx
@@ -1,7 +1,14 @@
 import { useState, useCallback, useRef } from 'react';
-import { LogLevels, Track, Room } from 'twilio-video';
+import { LogLevels, Track, Room, LocalAudioTrack, LocalVideoTrack, TrackPublication } from 'twilio-video';
 import { ErrorCallback } from '../../../types';
 import useLocalTracks from '../useLocalTracks/useLocalTracks';
+import {
+  SubscribeRulesInstance,
+  SubscribeRulesList,
+  SubscribeRulesPage,
+} from 'twilio/lib/rest/video/v1/room/roomParticipant/roomParticipantSubscribeRule';
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
+import { time } from 'console';
 
 interface MediaStreamTrackPublishOptions {
   name?: string;
@@ -19,39 +26,67 @@ export default function useScreenShareToggle(room: Room | null, onError: ErrorCa
   const shareScreen = useCallback(() => {
     navigator.mediaDevices
       .getDisplayMedia({
-        audio: false,
+        audio: true,
         video: true,
       })
       .then(stream => {
-        const track = stream.getTracks()[0];
+        //remove local microphone track
+        //room!.localParticipant.unpublishTrack(room!.localParticipant.getaudioTracks[0].track);
 
-        const tracks = stream.getTracks();
+        var micTrack = room!.localParticipant.tracks;
+        var mictrack: any;
+        micTrack.forEach(element => {
+          mictrack = element.track;
+          //remove mictrack
+          room!.localParticipant.unpublishTrack(element.track);
+        });
+        const audioTrack = mictrack.mediaStreamTrack;
+        const otherTrack = stream.getAudioTracks()[0];
+        debugger;
 
-        //tracks.forEach(track => {
-        // only publish if track kind is video
-        //if (track.kind === 'video') {
-        var streamName = track.kind;
+        var OutgoingAudioMediaStream = new MediaStream();
+        OutgoingAudioMediaStream.addTrack(otherTrack);
 
-        room!.localParticipant
-          .publishTrack(track, {
-            name: 'video', // Tracks can be named to easily find them later
-            priority: 'low', // Priority is set to high by the subscriber when the video track is rendered
-          } as MediaStreamTrackPublishOptions)
-          .then(trackPublication => {
-            stopScreenShareRef.current = () => {
-              room!.localParticipant.unpublishTrack(track);
-              // TODO: remove this if the SDK is updated to emit this event
-              room!.localParticipant.emit('trackUnpublished', trackPublication);
-              track.stop();
-              setIsSharing(false);
-            };
+        var IncomingAudioMediaStream = new MediaStream();
+        IncomingAudioMediaStream.addTrack(audioTrack);
 
-            track.onended = stopScreenShareRef.current;
-            setIsSharing(true);
-          })
-          .catch(onError);
-        //}
-        //});
+        const audioContext = new AudioContext();
+
+        var audioIn_01 = audioContext.createMediaStreamSource(OutgoingAudioMediaStream);
+        var audioIn_02 = audioContext.createMediaStreamSource(IncomingAudioMediaStream);
+
+        var dest = audioContext.createMediaStreamDestination();
+
+        audioIn_01.connect(dest);
+        audioIn_02.connect(dest);
+
+        var FinalStream = dest.stream;
+        stream = FinalStream;
+
+        //stream.addTrack(audioTrack);
+        var tracks = stream.getTracks();
+
+        tracks.forEach(track => {
+          room!.localParticipant
+            .publishTrack(track, {
+              name: track.kind + '-' + track.id,
+              priority: 'low', // Priority is set to high by the subscriber when the video track is rendered
+            } as MediaStreamTrackPublishOptions)
+            .then(trackPublication => {
+              stopScreenShareRef.current = () => {
+                room!.localParticipant.unpublishTrack(track);
+                // TODO: remove this if the SDK is updated to emit this event
+                room!.localParticipant.emit('trackUnpublished', trackPublication);
+                track.stop();
+                setIsSharing(false);
+              };
+
+              track.onended = stopScreenShareRef.current;
+              setIsSharing(true);
+            })
+            .catch(onError);
+          //}
+        });
       })
       .catch(error => {
         // Don't display an error if the user closes the screen share dialog
